31e8fdf2e69c6ff44a7de3dc1ed88029eab77950
ag597482 <ag597482@gmail.com>
2022-09-29 00:22:52 +0530

    added text types

--- lib/main.dart
+++ lib/main.dart
@@ -10,40 +10,20 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "first",
-      home: Scaffold(
-          backgroundColor: Color.fromARGB(255, 59, 255, 141),
-          appBar: AppBar(
-            backgroundColor: Color.fromARGB(255, 59, 203, 255),
-            title: Text("App Bar"),
-            leading: Icon(
-              Icons.home,
-              color: Colors.yellow,
+        title: "first",
+        home: Scaffold(
+            backgroundColor: Color.fromARGB(255, 59, 255, 141),
+            appBar: AppBar(
+              backgroundColor: Color.fromARGB(255, 59, 203, 255),
+              title: Text("App Bar"),
+              leading: Icon(
+                Icons.home,
+                color: Colors.yellow,
+              ),
             ),
-          ),
-          body: Container(
-            color: Colors.grey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    getContainer("t left", Colors.red),
-                    getContainer("t right", Colors.yellow)
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    getContainer("b left", Colors.blue),
-                    getContainer("b right", Colors.green)
-                  ],
-                )
-              ],
-            ),
-          )),
-    );
+            body: Column(
+              children: [getText("hello"), getRichText(), getTextField()],
+            )));
   }
 }
 
@@ -57,5 +37,67 @@ Container getContainer(var v, Color c) {
         border: Border.all(width: 4, color: Colors.black),
         color: c,
         borderRadius: BorderRadius.circular(50)),
+  );
+}
+
+Text getText(String s) {
+  return Text("Hello Coder's",
+      style: TextStyle(
+          color: Colors.pink,
+          fontSize: 30,
+          fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.w500,
+          backgroundColor: Colors.amberAccent,
+          letterSpacing: 4.0,
+          wordSpacing: 5.0,
+          shadows: [
+            Shadow(color: Colors.black, offset: Offset(3.0, 3.0)),
+            Shadow(color: Colors.blue, offset: Offset(4.0, 4.0))
+          ]));
+}
+
+RichText getRichText() {
+  return RichText(
+    text: TextSpan(
+        text: "hello",
+        style: TextStyle(
+            color: Colors.pink,
+            fontSize: 30,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.w500,
+            backgroundColor: Colors.amberAccent,
+            letterSpacing: 4.0,
+            wordSpacing: 5.0,
+            shadows: [
+              Shadow(color: Colors.black, offset: Offset(3.0, 3.0)),
+              Shadow(color: Colors.blue, offset: Offset(4.0, 4.0))
+            ]),
+        children: [
+          TextSpan(
+              text: "aman",
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 30,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w500,
+                  backgroundColor: Colors.amberAccent,
+                  letterSpacing: 4.0,
+                  wordSpacing: 5.0,
+                  shadows: [
+                    Shadow(color: Colors.black, offset: Offset(3.0, 3.0)),
+                    Shadow(color: Colors.blue, offset: Offset(4.0, 4.0))
+                  ]))
+        ]),
+  );
+}
+
+TextField getTextField() {
+  return TextField(
+    //cursorWidth: 2,
+    decoration: InputDecoration(
+        hoverColor: Colors.black,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        labelText: "Name",
+        hintText: "Enter you name"),
   );
 }
