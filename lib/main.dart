b16566e169b5acdd22b8572e2394296d325fc50b
ag597482 <ag597482@gmail.com>
2022-10-04 14:55:39 +0530

    icon addedD

--- lib/main.dart
+++ lib/main.dart
@@ -24,66 +24,12 @@ class App extends StatelessWidget {
               color: Colors.grey,
             ),
           ),
-          body: Forms(),
+          body: Icon(
+            Icons.delete,
+            color: Colors.black,
+            size: 40,
+            textDirection: TextDirection.ltr,
+          ),
         ));
   }
-}
-
-class Forms extends StatefulWidget {
-  @override
-  State<Forms> createState() => _FormsState();
-}
-
-class _FormsState extends State<Forms> {
-  final keys = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-        key: keys,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextFormField(
-              decoration: InputDecoration(
-                  hintText: "Enter your Name",
-                  labelText: "Name",
-                  icon: Icon(Icons.person)),
-              validator: ((value) => validate(value)),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                  hintText: "Enter password",
-                  labelText: "Password",
-                  icon: Icon(Icons.lock)),
-              validator: ((value) => validate(value)),
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                  hintText: "Enter github link",
-                  labelText: "Github Link",
-                  icon: Icon(Icons.person)),
-              validator: ((value) => validate(value)),
-              autocorrect: true,
-            ),
-            Container(
-              child: ElevatedButton(
-                  onPressed: () {
-                    if (keys.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text("validating text")));
-                    }
-                  },
-                  child: Text("Submit")),
-            )
-          ],
-        ));
-  }
-}
-
-String? validate(value) {
-  if (value == null || value.isEmpty) {
-    return "Please fill details";
-  }
-  return null;
 }
