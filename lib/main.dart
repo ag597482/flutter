c6412ba40ab1b12c1bd812c909d522f1619fc75f
ag597482 <ag597482@gmail.com>
2022-10-04 14:46:12 +0530

    form added

--- lib/main.dart
+++ lib/main.dart
@@ -15,52 +15,75 @@ class App extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: "first",
         home: Scaffold(
-            backgroundColor: Colors.grey,
-            appBar: AppBar(
-              backgroundColor: Colors.black,
-              title: Text("Stack"),
-              leading: Icon(
-                Icons.book,
-                color: Colors.grey,
-              ),
+          backgroundColor: Colors.grey,
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Text("Form"),
+            leading: Icon(
+              Icons.book,
+              color: Colors.grey,
             ),
-            body: Center(
-              child: Stack(
-                alignment: Alignment.center,
-                fit: StackFit.passthrough,
-                clipBehavior: Clip.hardEdge,
-                children: [
-                  getContainer(Colors.yellow, 200, 200, Container()),
-                  getContainer(
-                      Colors.orange,
-                      300,
-                      100,
-                      Center(
-                        child: Text(
-                          "hello !!!",
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w700),
-                        ),
-                      )),
-                  Positioned(
-                      top: 10,
-                      left: 50,
-                      child: getContainer(Colors.pink, 20, 20, Text("hi"))),
-                  Positioned(
-                      top: 10,
-                      right: 50,
-                      child: getContainer(Colors.pink, 20, 20, Text("hi")))
-                ],
-              ),
-            )));
+          ),
+          body: Forms(),
+        ));
   }
 }
 
-Container getContainer(Color color, double h, double w, Widget widget) {
-  return Container(
-    height: h,
-    width: w,
-    color: color,
-    child: widget,
-  );
+class Forms extends StatefulWidget {
+  @override
+  State<Forms> createState() => _FormsState();
+}
+
+class _FormsState extends State<Forms> {
+  final keys = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: keys,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Enter your Name",
+                  labelText: "Name",
+                  icon: Icon(Icons.person)),
+              validator: ((value) => validate(value)),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Enter password",
+                  labelText: "Password",
+                  icon: Icon(Icons.lock)),
+              validator: ((value) => validate(value)),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Enter github link",
+                  labelText: "Github Link",
+                  icon: Icon(Icons.person)),
+              validator: ((value) => validate(value)),
+              autocorrect: true,
+            ),
+            Container(
+              child: ElevatedButton(
+                  onPressed: () {
+                    if (keys.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("validating text")));
+                    }
+                  },
+                  child: Text("Submit")),
+            )
+          ],
+        ));
+  }
+}
+
+String? validate(value) {
+  if (value == null || value.isEmpty) {
+    return "Please fill details";
+  }
+  return null;
 }
